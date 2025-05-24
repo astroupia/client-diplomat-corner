f88354e47caf454c732b1b0cde94b5591120137c
Michael seyoum <104850692+itskarloos@users.noreply.github.com>
2025-05-24 14:29:02 +0300

    Bug fix: image in next.config

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,7 @@ const nextConfig: NextConfig = {
       "images.clerk.dev",
       "img.clerk.com",
       "uploadthing.com",
+      "media-api.media-api.diplomatcorner.net",
       "utfs.io",
       "files.stripe.com",
     ],
